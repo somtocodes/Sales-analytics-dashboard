3a2eafa6eef06284af5c1a3dd1eeb680485589a7
somtocodes <chukwunomnazusomto@gmail.com>
2026-05-13 19:54:42 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 st.set_page_config(
-	page_title = "Haven Analytics Dahsboard"
+	page_title = "Haven Analytics Dahsboard",
 	layout = 'wide'
 )
 
